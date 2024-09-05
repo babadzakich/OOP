9ee48c7cfb92171a4c08038864c8cb260408c48e
babadzakich <doctorare@mail.ru>
2024-09-05 10:10:45 +0700

    checkStyle fix 1

--- Task_1_1_1/src/test/java/ru/nsu/chuvashov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/chuvashov/MainTest.java
@@ -1,22 +1,23 @@
 package ru.nsu.chuvashov;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Random;
+import org.junit.jupiter.api.Test;
 
 
 
 class MainTest {
     @Test
-    void checkMain(){
-        Main.main(new String[]{});
+    void checkMain() {
+        Main.main(new String[] {});
         assertTrue(true);
     }
 
     @Test
-    void checkLargeArray(){
+    void checkLargeArray() {
         int[] expected = new int[10000000];
         Random r = new Random();
 
@@ -31,37 +32,37 @@ class MainTest {
     }
 
     @Test
-    void checkSingleElemArray(){
-        int[] expected = new int[]{1};
+    void checkSingleElemArray() {
+        int[] expected = new int[] {1};
 
-        int[] actual = Main.heapsort(new int[]{1});
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void checkEmptyArray(){
-        int[] expected = new int[]{};
-
-        int[] actual = Main.heapsort(new int[]{});
+        int[] actual = Main.heapsort(new int[] {1});
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    void checkSortedArray(){
-        int[] expected = new int[]{1,2,3,4,5,6};
+    void checkEmptyArray() {
+        int[] expected = new int[] {};
 
-        int[] actual = Main.heapsort(new int[]{1,2,3,4,5,6});
+        int[] actual = Main.heapsort(new int[] {});
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    void checkReverseSortedArray(){
-        int[] expected = new int[]{1,2,3,4,5,6};
+    void checkSortedArray() {
+        int[] expected = new int[] {1,2,3,4,5,6};
 
-        int[] actual = Main.heapsort(new int[]{6,5,4,3,2,1});
+        int[] actual = Main.heapsort(new int[] {1,2,3,4,5,6});
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void checkReverseSortedArray() {
+        int[] expected = new int[] {1,2,3,4,5,6};
+
+        int[] actual = Main.heapsort(new int[] {6,5,4,3,2,1});
 
         assertArrayEquals(expected, actual);
     }
