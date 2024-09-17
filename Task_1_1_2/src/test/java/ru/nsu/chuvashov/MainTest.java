c22c2831f875538a42ab3019f8631d8fd7391d6e
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2024-09-17 15:07:53 +0700

    practicing with tests

--- Task_1_1_2/src/test/java/ru/nsu/chuvashov/MainTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/chuvashov/MainTest.java
@@ -5,11 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import ru.nsu.chuvashov.bj.Main;
 
-class MainTest {
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
-//    @Test
-//    void checkMain() {
-//        Main.main(new String[] {});
-//        assertTrue(true);
-//    }
+class MainTest {
+    @Test
+    void checkMain() {
+        InputStream inapt = System.in;
+
+        try {
+            ByteArrayInputStream in = new ByteArrayInputStream("0\n".getBytes());
+            System.setIn(in);
+            Main.main(new String[] {});
+
+            assertTrue(true);
+        } catch (Exception e) {
+            ByteArrayInputStream in2 = new ByteArrayInputStream("no\n".getBytes());
+            System.setIn(in2);
+        } finally {
+            System.setIn(inapt);
+        }
+    }
 }
