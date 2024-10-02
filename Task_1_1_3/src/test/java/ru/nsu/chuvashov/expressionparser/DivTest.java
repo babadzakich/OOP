e8ddc9981357d981179e05466d3f0fa197502f6a
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2024-10-02 16:30:46 +0700

    added tests for division and fixin reviewDog

--- Task_1_1_3/src/test/java/ru/nsu/chuvashov/expressionparser/DivTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/chuvashov/expressionparser/DivTest.java
@@ -1,14 +1,17 @@
 package ru.nsu.chuvashov.expressionparser;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 class DivTest {
 
+    /**
+     * Test for dividing by zero.
+     */
     @Test
-    void eval() {
+    void eval1() {
         boolean flag = false;
         try {
             double result = new Div(new Number(5), new Sub(new Number(5), new Number(5))).eval("");
@@ -19,13 +22,46 @@ class DivTest {
         }
     }
 
+    /**
+     * Simple test for division.
+     */
     @Test
-    void print() {
+    void eval2() {
+        double result;
+        try {
+            result = new Div(new Number(10),
+                    new Add(new Variable("X"),
+                            new Number(3))).eval("X = 2");
+        } catch (Exception e) {
+            result = 1;
+        }
+
+        assertEquals(2, result);
+    }
+
+    /**
+     * Test for printing brackets.
+     */
+    @Test
+    void print1() {
         String expression = new Div(new Number(10),
                 new Mul(new Number(1), new Number(2))).toString();
         assertEquals("(10.0 / (1.0 * 2.0))", expression);
     }
 
+    /**
+     * Another brackets print test.
+     */
+    @Test
+    void print2() {
+        String expression = new Div(new Add(new Number(10), new Number(2)),
+                new Mul(new Variable("XA"), new Number(2))).toString();
+        assertEquals("((10.0 + 2.0) / (XA * 2.0))", expression);
+    }
+
+    /**
+     * Derivative calculating process.
+     */
     @Test
     void derivative() {
         Expression der = new Div(new Mul(new Number(5), new Variable("X")),
