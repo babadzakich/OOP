e93120b5d07e56f1a1a9c78f38ee247e38964fe5
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2024-11-10 23:15:50 +0700

    Fix test

--- Task_1_3_1/src/test/java/ru/nsu/chuvashov/substring/KnuthMorrisPrattTest.java
+++ Task_1_3_1/src/test/java/ru/nsu/chuvashov/substring/KnuthMorrisPrattTest.java
@@ -1,18 +1,22 @@
 package ru.nsu.chuvashov.substring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class KnuthMorrisPrattTest {
 
-  @Test
-  void getPattern() {
-    String text = "абракадабра";
-    String pattern = "бра";
-    List<Integer> result = new ArrayList<>();
-
-    KnuthMorrisPratt.getPattern(text, pattern, result);
-    System.out.println(result);
-  }
+    @Test
+    void getPattern() {
+        String text = "абракадабра";
+        String pattern = "бра";
+        List<Integer> result = new ArrayList<>();
+        KnuthMorrisPratt.getPattern(text, pattern, result);
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(8);
+        assertEquals(expected, result);
+    }
 }
