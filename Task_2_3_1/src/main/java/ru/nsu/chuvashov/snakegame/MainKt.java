b2cfb0e1d5aa6179572b56174f8d272b30f9b27a
Artyom Chuvashov <a.chuvashov@g.nsu.ru>
2025-04-01 23:42:31 +0700

    Change timeline to animationTimer

--- Task_2_3_1/src/main/java/ru/nsu/chuvashov/snakegame/MainKt.java
+++ Task_2_3_1/src/main/java/ru/nsu/chuvashov/snakegame/MainKt.java
@@ -1,5 +1,6 @@
 package ru.nsu.chuvashov.snakegame;
 
+import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -23,9 +24,12 @@ public class MainKt extends Application {
     private boolean moved = false;
     private boolean running = false;
 
-    private Timeline timeline = new Timeline();
+    private AnimationTimer animationTimer;
+    private long lastTime = 0;
+    private final double speed = 0.13 * 1_000_000_000L; // 0.2 секунды в наносекундах
 
     private ObservableList<Node> snake;
+    private Rectangle food;
 
     private Parent createContent() {
         Pane root = new Pane();
@@ -33,79 +37,87 @@ public class MainKt extends Application {
         Group snakeBody = new Group();
         snake = snakeBody.getChildren();
 
-        Rectangle food  = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
+        food  = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
         food.setFill(Color.BLUE);
         food.setTranslateX((int) (Math.random() * (BLOCK_WIDTH - BLOCK_SIZE)) / BLOCK_SIZE * BLOCK_SIZE);
         food.setTranslateY((int) (Math.random() * (BLOCK_HEIGHT - BLOCK_SIZE)) / BLOCK_SIZE * BLOCK_SIZE);
 
-        KeyFrame frame = new KeyFrame(Duration.seconds(0.2), event -> {
-            if (!running) {
-                return;
-            }
-
-            boolean toRemove = snake.size() > 1;
-
-            Node tail = toRemove ? snake.removeLast() : snake.getFirst();
-
-            double tailX = tail.getTranslateX();
-            double tailY = tail.getTranslateY();
-
-            switch (direction) {
-                case UP:
-                    tail.setTranslateX(snake.getFirst().getTranslateX());
-                    tail.setTranslateY(snake.getFirst().getTranslateY() - BLOCK_SIZE);
-                    break;
-                case DOWN:
-                    tail.setTranslateX(snake.getFirst().getTranslateX());
-                    tail.setTranslateY(snake.getFirst().getTranslateY() + BLOCK_SIZE);
-                    break;
-                case LEFT:
-                    tail.setTranslateX(snake.getFirst().getTranslateX() - BLOCK_SIZE);
-                    tail.setTranslateY(snake.getFirst().getTranslateY());
-                    break;
-                case RIGHT:
-                    tail.setTranslateX(snake.getFirst().getTranslateX() + BLOCK_SIZE);
-                    tail.setTranslateY(snake.getFirst().getTranslateY());
-                    break;
-            }
-
-            moved = true;
-
-            if (toRemove) {
-                snake.addFirst(tail);
-            }
-
-            for (Node node : snake) {
-                if (node != tail && tail.getTranslateX() == node.getTranslateX() && tail.getTranslateY() == node.getTranslateY()) {
-                    restartGame();
-                    break;
+        animationTimer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                if (!running) {
+                    return;
+                }
+                if (l - lastTime >= speed) {
+                    updateSnake();
+                    lastTime = l;
                 }
             }
-
-            if (tail.getTranslateX() < 0
-                    || tail.getTranslateY() < 0
-                    || tail.getTranslateX() >= BLOCK_WIDTH
-                    || tail.getTranslateY() >= BLOCK_HEIGHT ) {
-                restartGame();
-            }
-
-            if (tail.getTranslateX() == food.getTranslateX()
-                    && tail.getTranslateY() == food.getTranslateY()) {
-                food.setTranslateX((int) (Math.random() * (BLOCK_WIDTH - BLOCK_SIZE)) / BLOCK_SIZE * BLOCK_SIZE);
-                food.setTranslateY((int) (Math.random() * (BLOCK_HEIGHT - BLOCK_SIZE)) / BLOCK_SIZE * BLOCK_SIZE);
-
-                Rectangle rect = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
-                rect.setTranslateX(tailX);
-                rect.setTranslateY(tailY);
-                snake.add(rect);
-            }
-        });
-
-        timeline.getKeyFrames().add(frame);
-        timeline.setCycleCount(Timeline.INDEFINITE);
+        };
 
         root.getChildren().addAll(food, snakeBody);
         return root;
+    }
+
+    private void updateSnake() {
+
+
+        boolean toRemove = snake.size() > 1;
+
+        Node tail = toRemove ? snake.removeLast() : snake.getFirst();
+
+        double tailX = tail.getTranslateX();
+        double tailY = tail.getTranslateY();
+
+        switch (direction) {
+            case UP:
+                tail.setTranslateX(snake.getFirst().getTranslateX());
+                tail.setTranslateY(snake.getFirst().getTranslateY() - BLOCK_SIZE);
+                break;
+            case DOWN:
+                tail.setTranslateX(snake.getFirst().getTranslateX());
+                tail.setTranslateY(snake.getFirst().getTranslateY() + BLOCK_SIZE);
+                break;
+            case LEFT:
+                tail.setTranslateX(snake.getFirst().getTranslateX() - BLOCK_SIZE);
+                tail.setTranslateY(snake.getFirst().getTranslateY());
+                break;
+            case RIGHT:
+                tail.setTranslateX(snake.getFirst().getTranslateX() + BLOCK_SIZE);
+                tail.setTranslateY(snake.getFirst().getTranslateY());
+                break;
+        }
+
+        moved = true;
+
+        if (toRemove) {
+            snake.addFirst(tail);
+        }
+
+        for (Node node : snake) {
+            if (node != tail && tail.getTranslateX() == node.getTranslateX() && tail.getTranslateY() == node.getTranslateY()) {
+                restartGame();
+                break;
+            }
+        }
+
+        if (tail.getTranslateX() < 0
+                || tail.getTranslateY() < 0
+                || tail.getTranslateX() >= BLOCK_WIDTH
+                || tail.getTranslateY() >= BLOCK_HEIGHT) {
+            restartGame();
+        }
+
+        if (tail.getTranslateX() == food.getTranslateX()
+                && tail.getTranslateY() == food.getTranslateY()) {
+            food.setTranslateX((int) (Math.random() * (BLOCK_WIDTH - BLOCK_SIZE)) / BLOCK_SIZE * BLOCK_SIZE);
+            food.setTranslateY((int) (Math.random() * (BLOCK_HEIGHT - BLOCK_SIZE)) / BLOCK_SIZE * BLOCK_SIZE);
+
+            Rectangle rect = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
+            rect.setTranslateX(tailX);
+            rect.setTranslateY(tailY);
+            snake.add(rect);
+        }
     }
 
     private void restartGame() {
@@ -115,7 +127,7 @@ public class MainKt extends Application {
 
     private void stopGame() {
         running = false;
-        timeline.stop();
+        animationTimer.stop();
         snake.clear();
     }
 
@@ -123,8 +135,9 @@ public class MainKt extends Application {
         direction = Direction.RIGHT;
         Rectangle head = new Rectangle(BLOCK_SIZE, BLOCK_SIZE);
         snake.add(head);
-        timeline.play();
         running = true;
+        lastTime = System.nanoTime();
+        animationTimer.start();
     }
 
     @Override
